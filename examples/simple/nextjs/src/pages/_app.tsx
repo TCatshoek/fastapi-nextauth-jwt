1e426d88320fb34486f3addb711c74d56afc3789
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2023-07-17 18:27:38 +0200

    Fix Session not being included properly

--- examples/simple/nextjs/src/pages/_app.tsx
+++ examples/simple/nextjs/src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
 import {SessionProvider} from "next-auth/react"
+import {Session} from "next-auth";
 
-export default function App({Component, session, pageProps}: AppProps) {
+export default function App({Component, session, pageProps}: AppProps<{
+    session: Session;
+}>) {
     return (
         <SessionProvider session={session}>
             <Component {...pageProps} />
