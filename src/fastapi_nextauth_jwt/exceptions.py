79a8c98212125ac78e29ab7c08f461784b60e760
b0n3sh <b0n3sh@gmail.com>
2024-06-24 09:45:41 +0200

    replaced standard Exception with FastAPI HTTPException

--- src/fastapi_nextauth_jwt/exceptions.py
+++ src/fastapi_nextauth_jwt/exceptions.py
@@ -1,35 +1,37 @@
-class NextAuthJWTException(Exception):
+from fastapi import HTTPException
+
+class NextAuthJWTException(HTTPException):
     def __init__(self, *args: object):
         super().__init__(args)
-        self.message = None
+        self.detail = None
         self.status_code = None
 
 
 class MissingTokenError(NextAuthJWTException):
     def __init__(self, status_code: int, message: str):
         self.status_code = status_code
-        self.message = message
+        self.detail = message
 
 
 class InvalidTokenError(NextAuthJWTException):
     def __init__(self, status_code: int, message: str):
         self.status_code = status_code
-        self.message = message
+        self.detail = message
 
 
 class CSRFMismatchError(NextAuthJWTException):
     def __init__(self, status_code: int, message: str):
         self.status_code = status_code
-        self.message = message
+        self.detail = message
 
 
 class TokenExpiredException(NextAuthJWTException):
     def __init__(self, status_code: int, message: str):
         self.status_code = status_code
-        self.message = message
+        self.detail = message
 
 
 class UnsupportedEncryptionAlgorithmException(NextAuthJWTException):
     def __init__(self, status_code: int, message: str):
         self.status_code = status_code
-        self.message = message
+        self.detail = message
