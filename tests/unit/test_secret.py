dc4a23610e9e034ac8870ce858f580435b489d0b
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2025-05-24 13:37:12 +0200

    Add tests for deprecation warnings

--- tests/unit/test_secret.py
+++ tests/unit/test_secret.py
@@ -30,3 +30,21 @@ def test_obtain_secret_not_set():
     with pytest.raises(ValueError) as exc_info:
         NextAuthJWT()
         assert exc_info.value.message == "Secret not set"
+
+
+@pytest.mark.filterwarnings("error")
+def test_secret_auth_secret_no_url(monkeypatch):
+    monkeypatch.setenv("AUTH_SECRET", "foo")
+    with pytest.warns(RuntimeWarning, match="AUTH_URL not set"):
+        NextAuthJWT()
+
+def test_secret_nextauth_secret_deprecation_warning(monkeypatch):
+    monkeypatch.setenv("NEXTAUTH_SECRET", "foo")
+    with pytest.deprecated_call():
+        NextAuthJWT()
+
+def test_secret_nextauth_url_deprecation_warning(monkeypatch):
+    monkeypatch.setenv("AUTH_SECRET", "foo")
+    monkeypatch.setenv("NEXTAUTH_URL", "bar")
+    with pytest.deprecated_call():
+        NextAuthJWT()
